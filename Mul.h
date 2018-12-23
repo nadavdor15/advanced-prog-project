3d83654b2c06f2cafa2068f044b8dffd8825964b
nadavdor15 <46018347+nadavdor15@users.noreply.github.com>
2018-12-23 19:12:00 +0200

    Update Mul.h

--- Mul.h
+++ Mul.h
@@ -6,8 +6,7 @@ class Mul : public BinaryExpression {
 public:
     Mul(Expression& right_expression,
         Expression& left_expression):
-        BinaryExpression(right_expression, 
-                         left_expression) {}
+        BinaryExpression(right_expression, left_expression) {}
 
     double calculate(vector<string>& arguments, int index) {
         return _right_expression.calculate(arguments, index) *
