9391e54093c4da5bb84a9a6131ab374e43a98f09
nadavdor15 <46018347+nadavdor15@users.noreply.github.com>
2018-12-23 19:07:47 +0200

    Update Mul.h

--- Mul.h
+++ Mul.h
@@ -1,10 +1,15 @@
 #include "BinaryExpression.h"
 
+using namespace std;
+
 class Mul : public BinaryExpression {
 public:
     Mul(Expression& right_expression,
         Expression& left_expression):
         BinaryExpression(right_expression, 
                          left_expression) {}
-    double calculate();
+    double calculate(vector<string>& arguments, int index) {
+        return _right_expression.calculate(arguments, index) *
+               _left_expression.calculate(arguments, index);
+    }
 };
