1462c891a6010221ba37f3a626588856663fbe79
nadavdor15 <46018347+nadavdor15@users.noreply.github.com>
2018-12-23 18:57:29 +0200

    Update Number.h

--- Number.h
+++ Number.h
@@ -1,14 +1,14 @@
-#ifndef APP_NUMBER_H
-#define APP_NUMBER_H
-
 #include "Expression.h"
 
 class Number : public Expression {
 private:
     double _value;
 public:
-    double calculate();
-    Number(double v);
-};
+    Number(double value) {
+        _value = value;
+    }
 
-#endif //APP_NUMBER_H
+    virtual double calculate(vector<string>& arguments, int index) {
+        return _value;
+    }
+};
