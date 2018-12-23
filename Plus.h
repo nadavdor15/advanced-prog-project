e9a9a03ac9157151beab9447e42aac76fe5c2238
nadavdor15 <46018347+nadavdor15@users.noreply.github.com>
2018-12-23 19:17:20 +0200

    Update Plus.h

--- Plus.h
+++ Plus.h
@@ -1,13 +1,15 @@
-#ifndef APP_MINUS_H
-#define APP_MINUS_H
-
-
 #include "BinaryExpression.h"
+
+using namespace std;
 
 class Plus : public BinaryExpression {
 public:
-    Plus(Expression * a, Expression * b);
-    double calculate();
-};
+    Plus(Expression& right_expression,
+          Expression& left_expression):
+          BinaryExpression(right_expression, left_expression) {}
 
-#endif
+    double calculate(vector<string>& arguments, int index) {
+        return _right_expression.calculate(arguments, index) +
+               _left_expression.calculate(arguments, index);
+    }
+};
