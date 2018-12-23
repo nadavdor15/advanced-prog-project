b293b59752bb905e786e1ff45738e95c2bbc8618
nadavdor15 <46018347+nadavdor15@users.noreply.github.com>
2018-12-23 19:13:28 +0200

    Update Div.h

--- Div.h
+++ Div.h
@@ -1,15 +1,15 @@
-#ifndef APP_DEV_H
-#define APP_DEV_H
-
-
 #include "BinaryExpression.h"
 
+using namespace std;
 
 class Div : public BinaryExpression {
 public:
-    Div(Expression * a, Expression * b);
-    double calculate();
+    Div(Expression& right_expression,
+          Expression& left_expression):
+          BinaryExpression(right_expression, left_expression) {}
+
+    double calculate(vector<string>& arguments, int index) {
+        return _right_expression.calculate(arguments, index) /
+               _left_expression.calculate(arguments, index);
+    }
 };
-
-
-#endif //APP_DEV_H
